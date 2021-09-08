bb4846f2dfe86935b4c6602b74b47983c30f2826
Aalia Habib <aalia_habib@brown.edu>
2021-09-08 11:30:57 -0400

    finally working ish

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# voxel_carving
+# Voxel Carving
+
+For my starter project I tried to implement the voxel carving algorithm outlined in this paper. 
+I used the temple dataset from the Middlebury Multi-View stereo data set. 
+
+To build the camera matrices, I used the camera information provided in the data set. 
+I had initially tried to build my own dataset using screenshots from Meshlab and the camera information 
+that they allow access to. However, I wasn't able to parse the xml camera information that they provided
+correctly and was ending up with wonky carvings. 
+
+Switching over to the temple dataset made building the camera matrix easier. However, iterating 
+over all the images causes the algorithm I wrote to carve all of the voxels. I'm not sure if this is a rounding
+issue or something else because it seems to work correctly for any set of 1-10 images. 
+
+I'm using open3D to render a point cloud of voxels that are left once my algorithm finishes iterating. 
+
+## Results
+Here are my results after iterating over 10 of the images in the dataset
+It looks a little wonky and I would think it might be a rounding
+error of some sort becuase the appearance of any one carving looks correct (figure 2).
+
+![Figure 1](results/temple1.png)
+![Figure 1](results/temple2.png)
+![Figure 1](results/temple3.png)
+![Figure 1](results/temple4.png)
+
+![Figure 2](results/figure2.png)
+
+
+## References
+[Original Voxel Carving paper](https://www.cs.toronto.edu/~kyros/pubs/00.ijcv.carve.pdf)
+
+[Image sillhouette extraction](http://creativemorphometrics.co.vu/blog/2014/08/05/automated-outlines-with-opencv-in-python)
+
+[Camera matrix breakdown](http://ksimek.github.io/2012/08/14/decompose/) 
--- voxelcarver.py
+++ voxelcarver.py
@@ -1,82 +1,76 @@
 import numpy as np
-from matplotlib import image
-from matplotlib import pyplot as plt
 import open3d as o3d
-import xml.etree.ElementTree as ET
+import cv2
 
-voxel_dim = 50
+BASE_DIR = "./temple/temple"
+PARAMS = open("./temple/temple_par.txt").readlines()
+
+sum = 0
+voxel_dim = 200
+voxel_size = 0.005
 voxel_grid = np.ones((voxel_dim, voxel_dim, voxel_dim))
 
-def parseCameraFile(filepath):
-    tree = ET.parse(filepath)
-    root = tree.getroot()
-    cam = root[0].attrib
+def extract_foreground_from_image(num):
+    # reference: http://creativemorphometrics.co.vu/blog/2014/08/05/automated-outlines-with-opencv-in-python
+    image = cv2.cvtColor(cv2.imread(BASE_DIR+str(format(num, "04"))+".png"), cv2.COLOR_BGR2GRAY)
+    r, threshold = cv2.threshold(image,10,255,cv2.THRESH_BINARY)
+    kernel = np.ones((5,5),np.uint8)
+    img = cv2.morphologyEx(cv2.erode(threshold, kernel,iterations = 1), cv2.MORPH_OPEN, kernel)
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    # plt.imshow(img, 'gray')
+    # plt.show()
+    # img = Image.open(BASE_DIR+str(format(num, "04"))+".png").convert('1').filter(ImageFilter.BLUR).filter(ImageFilter.MinFilter(3)).filter(ImageFilter.MinFilter)
+    img = np.asarray(img, dtype='float32')
+    return img
 
-    #translation vector
-    t = cam.get('TranslationVector')
-    t = np.array(t.split()).astype(float)
-    t = np.reshape(t, (4, 1))
-
-    #rotation matrix
-    r = cam.get('RotationMatrix')
-    r = np.array(r.split()).astype(float)
-    r = np.reshape(r, (4, 4))
-
-    Rt = np.hstack((r[:,0:3], t))
-
-    f = float(cam.get('FocalMm'))
-    o = cam.get('CenterPx')
-    o = np.array(o.split()).astype(float)
-
-    K = np.asmatrix(np.reshape(np.array([f, 0, o[0], 0, 0, f, o[1], 0, 0, 0, 1, 0]), (3, 4)))
-
-    return np.dot(K, Rt)
+def build_matrices(num):
+    camdata = PARAMS[num+1].split()[1:]
+    K = np.reshape(np.array(camdata[0:9]).astype(float), (3, 3))
+    R = np.reshape(np.array(camdata[9:18]).astype(float), (3, 3))
+    t = np.reshape(np.array(camdata[18:21]).astype(float), (3, 1))
+    return K, R, t
 
 
 
-def carve(filename):
-    cameraFile = filename+".xml"
-    imgFile = filename+".png"
+num = 1
 
-    M = parseCameraFile(cameraFile)
+for i in range(0, 10):
+    img = extract_foreground_from_image(num)
+    K, R, t = build_matrices(num)
 
-    img = image.imread(imgFile)
-    image_dim = img.shape
+    print("done setting camera params")
 
-    sum = 0
     for i in range(0, voxel_dim):
         for j in range(0, voxel_dim):
             for k in range(0, voxel_dim):
-                v = np.reshape(np.array([i, j, k, 1]), (4, 1))
-                projected_v = np.dot(M, v)
+                v = np.reshape(np.array([i*voxel_size, j*voxel_size, k*voxel_size]), (3, 1))
+                projected_v = np.dot(R, v) + t
+                projected_v = np.dot(K, projected_v)
+                v[0] = v[0]/v[2]
+                v[1] = v[1]/v[2]
                 # print(projected_v)
-                if (projected_v[0] > 0 and projected_v[1] > 0 and projected_v[0] < image_dim[0] and projected_v[1] < image_dim[1]):
-                    projected_v = projected_v.astype(int)
+                projected_v = np.round(projected_v).astype(int)
+                if (projected_v[0] > 0 and projected_v[1] > 0 and projected_v[0] < img.shape[0] and projected_v[1] < img.shape[1]):
                     sum = sum+1
-                    #print(img[projected_v[0], projected_v[1]])
                     if img[projected_v[0], projected_v[1]].sum() == 0:
                         #if the background is black, then carve
                         voxel_grid[i, j, k] = 0
                 else:
-                    #if the background is black, then carve
+                    #if the voxels are outside the image bounds, then carve
                     voxel_grid[i, j, k] = 0
 
     print(voxel_grid.sum())
     print(sum)
+    num=num+1
 
 
-carve("/Users/aalia/Desktop/bean_img01")
-# carve("/Users/aalia/Desktop/bean_img02")
 points = []
 for i in range(0, voxel_dim):
     for j in range(0, voxel_dim):
         for k in range(0, voxel_dim):
             if voxel_grid[i, j, k] > 0:
-                points.append([i, j, k])
-
-points = np.matrix(points, dtype='float')
-print(points.shape)
-
+                points.append([i*voxel_size, j*voxel_size, k*voxel_size])
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(points)
 o3d.visualization.draw_geometries([pcd])
+
